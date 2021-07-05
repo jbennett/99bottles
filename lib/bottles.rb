eb610be10b66cc5ed10989909e876bce2ebe4822
Jonathan Bennett <jonathan@jbennett.me>
2021-07-05 00:47:40 +0000

    open/closed

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -16,12 +16,17 @@ class Bottles
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\n" + 
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\n" + 
-      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
     else
       "#{x} bottles of beer on the wall, #{x} bottles of beer.\n" + 
-      "Take one down and pass it around, #{x - 1} bottles of beer on the wall.\n"
+      "Take one down and pass it around, #{x - 1} #{container(x - 1)} of beer on the wall.\n"
+    end
+  end
+  
+  def container(x)
+    if x == 1
+      "bottle"
+    else
+      "bottles"
     end
   end
 end
