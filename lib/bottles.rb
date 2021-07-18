484102919e0b93a069ddd38ca6cdf834b77c5931
Jonathan Bennett <jonathan@jbennett.me>
2021-07-18 08:54:23 -0400

    missed branch

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,37 +3,33 @@ class Bottles
   def song
     verses(99, 0)
   end
-  
+
   def verses(a, b)
     (b..a).map { |x| verse(x) }.reverse.join("\n")
   end
-  
+
   def verse(x)
-    "#{quantity(x).capitalize} #{container(x)} of beer on the wall, #{quantity(x)} #{container(x)} of beer.\n" + 
+    "#{quantity(x).capitalize} #{container(x)} of beer on the wall, #{quantity(x)} #{container(x)} of beer.\n" +
     "#{action(x)}, #{quantity(successor(x))} #{container(successor(x))} of beer on the wall.\n"
   end
-  
+
   def successor(x)
-    if x == 0
-      99
-    else
-      x - 1
-    end
+    BottleNumber.new(x).successor
   end
-  
+
   def action(x)
     BottleNumber.new(x).action
   end
-    
-  
+
+
   def quantity(x)
     BottleNumber.new(x).quantity
   end
-  
+
   def container(x)
     BottleNumber.new(x).container
   end
-  
+
   def pronoun(x)
     BottleNumber.new(x).pronoun
   end
@@ -41,19 +37,19 @@ end
 
 class BottleNumber
   attr_reader :number
-  
+
   def initialize(number)
     @number = number
   end
-  
+
   def successor
-    if x == 0
+    if number == 0
       99
     else
       number - 1
     end
   end
-  
+
   def action
     if number == 0
       "Go to the store and buy some more"
@@ -61,8 +57,7 @@ class BottleNumber
       "Take #{pronoun} down and pass it around"
     end
   end
-    
-  
+
   def quantity
     if number == 0
       "no more"
@@ -70,7 +65,7 @@ class BottleNumber
       number.to_s
     end
   end
-  
+
   def container
     if number == 1
       "bottle"
@@ -78,7 +73,7 @@ class BottleNumber
       "bottles"
     end
   end
-  
+
   def pronoun
     if number == 1
       "it"
@@ -93,37 +88,37 @@ end
 #   def song
 #     verses(99, 0)
 #   end
-#   
+#
 #   def verses(starting, ending)
 #     (ending..starting).map { |x| verse(x) }.reverse.join("\n")
 #   end
-#   
+#
 #   def verse(x)
 #     [
 #       current_bottle_phrase(x),
 #       next_bottle_phrase(x-1)
 #     ].join("\n") + "\n"
 #   end
-#   
+#
 #   def current_bottle_phrase(x)
 #     "#{bottle_desc(x).capitalize} of beer on the wall, #{bottle_desc(x)} of beer."
 #   end
-#   
+#
 #   def next_bottle_phrase(x)
 #     return last_bottle_phrase if x == -1
 #     "Take #{bottle_count(x)} down and pass it around, #{bottle_desc(x)} of beer on the wall."
 #   end
-#   
+#
 #   def last_bottle_phrase
 #     "Go to the store and buy some more, 99 bottles of beer on the wall."
 #   end
-#   
+#
 #   def bottle_desc(x)
 #     return "no more bottles" if x == 0
 #     return "1 bottle" if x == 1
 #     "#{x} bottles"
 #   end
-#   
+#
 #   def bottle_count(x)
 #     x == 0 ? "it" : "one"
 #   end
